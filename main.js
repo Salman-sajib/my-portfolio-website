e3ea0b36500978999517e166515a33d0090ac877
Salman-sajib <sfsajib@gmail.com>
2024-03-05 22:45:59 +0600

    completed the skills section

--- main.js
+++ main.js
@@ -1,4 +1,8 @@
 import { gsap } from 'gsap';
+import { ScrollTrigger } from 'gsap/ScrollTrigger';
+import Lenis from '@studio-freight/lenis';
+
+gsap.registerPlugin(ScrollTrigger);
 
 // Animate each letter of the greeting
 gsap.from('.greeting span', {
@@ -17,6 +21,33 @@ gsap.from('.about-me', {
   delay: 1, // Delay the animation for a stagger effect
 });
 
+// GSAP animation for the title
+gsap.from('.title', {
+  opacity: 0,
+  y: -50,
+  duration: 1,
+  scrollTrigger: {
+    trigger: '.title',
+    start: 'top 80%', // Adjust the start position as needed
+    end: 'bottom 20%', // Adjust the end position as needed
+    scrub: 1, // Smoothly animate the position during the scroll
+  },
+});
+
+// GSAP animation for the technology logos
+gsap.from('.technology-logos img', {
+  opacity: 0,
+  y: 50,
+  stagger: 0.2,
+  duration: 1,
+  scrollTrigger: {
+    trigger: '.technology-logos',
+    start: 'top 80%', // Adjust the start position as needed
+    end: 'bottom 80%', // Adjust the end position as needed
+    scrub: 1, // Smoothly animate the position during the scroll
+  },
+});
+
 // lenis for smooth scroll
 const lenis = new Lenis();
 
@@ -31,3 +62,19 @@ gsap.ticker.add((time) => {
 });
 
 gsap.ticker.lagSmoothing(0);
+
+document.addEventListener('DOMContentLoaded', function () {
+  // Select the "Skills" navigation link
+  const skillsLink = document.querySelector('a[href="#skills"]');
+
+  // Add a click event listener to the "Skills" link
+  skillsLink.addEventListener('click', function (event) {
+    event.preventDefault();
+
+    // Get the target element (Skills section)
+    const skillsSection = document.getElementById('skills');
+
+    // Scroll to the target element smoothly
+    skillsSection.scrollIntoView({ behavior: 'smooth' });
+  });
+});
